8888ccf057ebe07f1c037e5a39a9874e18ebd336
arnaudcoj <arnaud.cojez@etudiant.univ-lille1.fr>
2015-01-13 12:17:21 +0100

    dragon

---
+++ dragon.hs
@@ -0,0 +1,24 @@
+import Graphics.Gloss
+
+main = animate (InWindow "Dragon" (500, 500) (0, 0)) white (dragonAnime (50,250) (450,250))
+
+dragonAnime a b t = Line (dragon a b !! (round t `mod` 20))
+
+--Q5
+
+pointAintercaler :: Point -> Point -> Point
+
+pointAintercaler (xA, yA) (xB, yB) = ((xA + xB)/2 + (yB - yA)/2, (yA + yB)/2 + (xA - xB)/2)
+
+--Q6
+
+pasDragon :: Path -> Path
+
+pasDragon (xA:xB:xs) = xA:(pointAintercaler xA xB):[xB]
+pasDragon (xA:xB:xC:xs) = xA:(pointAintercaler xA xB):xB:(pointAintercaler xC xB):(pasDragon xC:xs)
+
+--Q7
+
+dragon :: Point -> Point -> [Path]
+
+dragon xA xB = iterate pasDragon [xA,xB]
---
+++ tp2.hs
@@ -0,0 +1,28 @@
+--Q1
+
+alterne :: [t] -> [t]
+
+alterne [] = []
+alterne (x:[]) = [x]
+alterne (x1:x2:xs) = x1:(alterne xs)
+
+--Q2
+
+combine :: (a -> b -> c) -> [a] -> [b] -> [c]
+
+combine _ _ []  = []
+combine _ [] _  = []
+combine f (x:xs) (y:ys) = (f x y):(combine f xs ys)
+                          
+--Q3
+
+pasPascal :: [Integer] -> [Integer]
+
+pasPascal xs = zipWith (+) (xs++[0]) (0:xs) 
+
+--Q4
+
+pascal :: [[Integer]]
+
+pascal = iterate pasPascal [1]
+
